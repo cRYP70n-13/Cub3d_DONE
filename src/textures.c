0a97564f94048cb8f8efb09bacfc2beb2f09b894
cRYP70n-13 <otmane.kimdil@gmail.com>
2021-01-16 11:59:52 +0000

    Just some other modif and a segfault

--- src/textures.c
+++ src/textures.c
@@ -6,7 +6,7 @@
 /*   By: okimdil <okimdil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 19:18:33 by okimdil           #+#    #+#             */
-/*   Updated: 2021/01/15 20:00:01 by okimdil          ###   ########.fr       */
+/*   Updated: 2021/01/16 12:58:40 by okimdil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 
 float	sean(t_mapdata *map)
 {
-	if (map->map2d[(int)map->playery / g_tiles][(int)map->playerx / g_tiles] == 'N')
+	if (map->map2d[(int)map->playery / g_tiles]
+					[(int)map->playerx / g_tiles] == 'N')
 		return (RAD(270));
-	else if (map->map2d[(int)map->playery / g_tiles][(int)map->playerx / g_tiles] == 'W')
+	else if (map->map2d[(int)map->playery / g_tiles]
+						[(int)map->playerx / g_tiles] == 'W')
 		return (RAD(180));
-	else if (map->map2d[(int)map->playery / g_tiles][(int)map->playerx / g_tiles] == 'S')
+	else if (map->map2d[(int)map->playery / g_tiles]
+						[(int)map->playerx / g_tiles] == 'S')
 		return (RAD(90));
-	else if (map->map2d[(int)map->playery / g_tiles][(int)map->playerx / g_tiles] == 'E')
+	else if (map->map2d[(int)map->playery / g_tiles]
+						[(int)map->playerx / g_tiles] == 'E')
 		return (RAD(0));
 	return (0);
 }
@@ -43,16 +47,20 @@ void	texture(t_mapdata *map)
 	int	usless;
 	int uslesser;
 
-	(map->textureimage = mlx_xpm_file_to_image(map->mlx, map->east, &uslesser, &uslesser))
-		? 0 : ft_error("wrong ea path");
-	map->texturedata[0] = (int *)mlx_get_data_addr(map->textureimage, &usless, &usless, &usless);
-	(map->textureimage = mlx_xpm_file_to_image(map->mlx, map->north, &uslesser, &uslesser))
-		? 0 : ft_error("wrong no path");
-	map->texturedata[1] = (int *)mlx_get_data_addr(map->textureimage, &usless, &usless, &usless);
-	(map->textureimage = mlx_xpm_file_to_image(map->mlx, map->west, &uslesser, &uslesser))
-		? 0 : ft_error("wrong we path");
-	map->texturedata[2] = (int *)mlx_get_data_addr(map->textureimage, &usless, &usless, &usless);
-	(map->textureimage = mlx_xpm_file_to_image(map->mlx, map->south, &uslesser, &uslesser))
-		? 0 : ft_error("wrong so path");
-	map->texturedata[3] = (int *)mlx_get_data_addr(map->textureimage, &usless, &usless, &usless);
+	(map->textureimage = mlx_xpm_file_to_image(map->mlx, map->east,
+			&uslesser, &uslesser)) ? 0 : ft_error("wrong ea path");
+	map->texturedata[0] = (int *)mlx_get_data_addr(map->textureimage,
+							&usless, &usless, &usless);
+	(map->textureimage = mlx_xpm_file_to_image(map->mlx, map->north,
+						&uslesser, &uslesser)) ? 0 : ft_error("wrong no path");
+	map->texturedata[1] = (int *)mlx_get_data_addr(map->textureimage,
+						&usless, &usless, &usless);
+	(map->textureimage = mlx_xpm_file_to_image(map->mlx, map->west,
+						&uslesser, &uslesser)) ? 0 : ft_error("wrong we path");
+	map->texturedata[2] = (int *)mlx_get_data_addr(map->textureimage,
+						&usless, &usless, &usless);
+	(map->textureimage = mlx_xpm_file_to_image(map->mlx, map->south,
+						&uslesser, &uslesser)) ? 0 : ft_error("wrong so path");
+	map->texturedata[3] = (int *)mlx_get_data_addr(map->textureimage,
+						&usless, &usless, &usless);
 }
